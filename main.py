218650c795262ec98aabd02c3a842ee903c87608
zeroxx1986 <zeroxx1986@gmail.com>
2020-07-30 14:54:05 +0200

    Added (unit)testing capability via pytest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telegram
-from creds import Telegram_API_Key
+from creds import creds
 
-bot = telegram.Bot(token=Telegram_API_Key())
+bot = telegram.Bot(token=creds.Telegram_API_Key())
 print(bot.get_me())
