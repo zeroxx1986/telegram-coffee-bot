a6464d744107dcf4d620ec88dad853b54af24155
zeroxx1986 <zeroxx1986@gmail.com>
2020-09-09 20:29:09 +0000

    trying to add 9gag translator

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from bot import bot as Bot
+import requests
+from lxml import html
 
-bot = Bot.Get()
+url = 'https://9gag.com/gag/amvMvQ2'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-print(bot.get_me())
+response = requests.get(url, headers=headers)
+tree = html.fromstring(response.content)
+
+image = tree.xpath('//link[@rel="image_src"]/@href')
+if len(image) > 0:
+    image_data = requests.get(image[0]).content
+    print(image_data)
+# print(image)
+
+# bot = Bot.Get()
+
+# print(bot.get_me())
 print("testing")
